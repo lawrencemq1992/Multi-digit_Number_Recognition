898aafe6f4cc9231a46238d1f9ce59ff75d007aa
lawrencemq1992 <lawrencemq1992@gmail.com>
2017-12-18 08:15:42 -0500

    models

--- models1.py
+++ models1.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
-from utils import Utils
 
-class Models(object):
-    
+
+class Model1(object):
+
     @staticmethod
     def evaluate(path_results, path_tfrecords, num_examples, tf_step_result):
         # this function is to make validation and evaluate accuracy
@@ -40,6 +40,7 @@ class Models(object):
     
     @staticmethod
     def cnn_loss(length, digits, length_batch, digits_batch):
+        # function as a minimizer calculator
         cross_entropys = []
         # length cross entropy
         cross_entropys.append(
@@ -48,7 +49,7 @@ class Models(object):
             cross_entropys.append(
                 tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(labels=digits[:, i], logits=digits_batch[:, i, :])))
         return sum(cross_entropys)
-
+    
     @staticmethod
     def cnn_inference(image, drop_rate):
         # feedforward
@@ -60,7 +61,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=2, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden1 = dropout
-
         with tf.variable_scope('hidden2'):
             conv = tf.layers.conv2d(hidden1, filters=64, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -68,7 +68,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=1, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden2 = dropout
-
         with tf.variable_scope('hidden3'):
             conv = tf.layers.conv2d(hidden2, filters=128, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -76,7 +75,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=2, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden3 = dropout
-
         with tf.variable_scope('hidden4'):
             conv = tf.layers.conv2d(hidden3, filters=160, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -84,7 +82,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=1, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden4 = dropout
-
         with tf.variable_scope('hidden5'):
             conv = tf.layers.conv2d(hidden4, filters=192, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -92,7 +89,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=2, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden5 = dropout
-
         with tf.variable_scope('hidden6'):
             conv = tf.layers.conv2d(hidden5, filters=192, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -100,7 +96,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=1, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden6 = dropout
-
         with tf.variable_scope('hidden7'):
             conv = tf.layers.conv2d(hidden6, filters=192, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -108,7 +103,6 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=2, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden7 = dropout
-
         with tf.variable_scope('hidden8'):
             conv = tf.layers.conv2d(hidden7, filters=192, kernel_size=[5, 5], padding='same')
             norm = tf.layers.batch_normalization(conv)
@@ -116,31 +110,32 @@ class Models(object):
             pool = tf.layers.max_pooling2d(activation, pool_size=[2, 2], strides=1, padding='same')
             dropout = tf.layers.dropout(pool, rate=drop_rate)
             hidden8 = dropout
-            
-        flatten = tf.reshape(hidden, [-1, 3072])  # 3072 = 4 * 4 * 192
-        
+        flatten = tf.reshape(hidden8, [-1, 4 * 4 * 192])  # 3072 = 4 * 4 * 192
         with tf.variable_scope('hidden9'):
-            dense = tf.layers.dense(flatten, units=3072, activation=tf.nn.relu)
+            dense = tf.layers.dense(flatten, units=3072, activation=tf.nn.sigmoid)
             hidden9 = dense
         with tf.variable_scope('hidden10'):
             dense = tf.layers.dense(hidden9, units=3072, activation=tf.nn.relu)
             hidden10 = dense
+        with tf.variable_scope('hidden11'):
+            dense = tf.layers.dense(hidden10, units=3072, activation=tf.nn.relu)
+            hidden11 = dense
         with tf.variable_scope('digit_length'):
-            dense = tf.layers.dense(hidden10, units=7)
+            dense = tf.layers.dense(hidden11, units=7)
             digit_length = dense
         with tf.variable_scope('digit1'):
-            dense = tf.layers.dense(hidden10, units=11)
+            dense = tf.layers.dense(hidden11, units=11)
             digit1 = dense
         with tf.variable_scope('digit2'):
-            dense = tf.layers.dense(hidden10, units=11)
+            dense = tf.layers.dense(hidden11, units=11)
             digit2 = dense
         with tf.variable_scope('digit3'):
-            dense = tf.layers.dense(hidden10, units=11)
+            dense = tf.layers.dense(hidden11, units=11)
             digit3 = dense
         with tf.variable_scope('digit4'):
-            dense = tf.layers.dense(hidden10, units=11)
+            dense = tf.layers.dense(hidden11, units=11)
             digit4 = dense
         with tf.variable_scope('digit5'):
-            dense = tf.layers.dense(hidden10, units=11)
+            dense = tf.layers.dense(hidden11, units=11)
             digit5 = dense
         return digit_length, tf.stack([digit1, digit2, digit3, digit4, digit5], axis=1)
